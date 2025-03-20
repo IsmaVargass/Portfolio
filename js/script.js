6eca12a3dfd68cc23a720b62a40228acb4d51b97
Ismael <ismaelvargasduque14@gmail.com>
2025-03-20 18:22:36 +0100

    ADVANCE -- Add Script.js, add script fade-up. Update css, reduced the file size of the video.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 "use strict"
+
+// Este script sirve para mostrar el contenido de la web lentamente según vaya siendo visible.
+
+// Este JS se puede intentar hacer en CSS solamente pero muchos navegadores lo no soporta. Modifico el threshold aumentándolo.
+
+// Añado el defer en el script de js que obliga ser ejecutado después de que el documento HTML se haya completado
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fadeElements = document.querySelectorAll('.fade-up');
+  
+    // Usamos IntersectionObserver para detectar cuándo un elemento entra en la vista
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Cuando el elemento entra en la vista, se le añade la clase 'show' para activar la animación
+          entry.target.classList.add('show');
+        }
+      });
+    }, { threshold: 0.3 }); // Umbral ajustado para activar el efecto cuando un 30% del elemento es visible
+  
+    fadeElements.forEach(el => observer.observe(el));
+  });
+  
+  
